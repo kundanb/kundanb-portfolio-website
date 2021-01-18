dc0bcdc185787e6978b6f5158501d71e1a72eaf5
Kundan Bhasin <kundanbhasin11062000@gmail.com>
2021-01-19 00:33:59 +0530

    setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+sass.compiler = require('node-sass');
+
+const cleanBuild = () => del('build');
+
+const buildCSS = () =>
+  gulp
+    .src('src/scss/**/*')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('build/css'));
+
+const buildJS = () =>
+  gulp
+    .src('src/js/**/*')
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+
+const buildImg = () =>
+  gulp
+    .src('src/js/**/*')
+    .pipe(imagemin({ interlaced: true }))
+    .pipe(gulp.dest('build/img'));
+
+const build = gulp.series(
+  cleanBuild,
+  gulp.parallel(buildCSS, buildJS, buildImg)
+);
+
+const watch = () => {
+  gulp.watch(['src/**/*'], build);
+};
+
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
